043cf9a100fd78a2392fb3389dbafb36bb6ca998
Pitipon Pluemworasawat <pitipon@gmail.com>
2018-11-12 13:38:59 +0800

    try

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 
 const publicPath = path.join(__dirname, '../public')
+console.log(333, publicPath)
 const port = process.env.PORT || 3000
 var app = express()
 
